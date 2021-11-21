e9da92e95b6a32270bb8b834d2f0c200ed18937d
j-devel <j@w3reality.com>
2021-11-21 12:43:00 +0700

    Add test data

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,19 @@
+use crate::{Voucher, SignatureAlgorithm, Sign, Validate};
+
+static VOUCHER_JADA: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/jada/voucher_jada123456789.vch"));
+
+static VOUCHER_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/voucher_00-D0-E5-F2-00-02.vch"));
+
+static MASA_CRT_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/masa.crt"));
+
+static KEY_PEM_02_00_2E: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-02-00-2E/key.pem"));
+
+static DEVICE_CRT_02_00_2E: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-02-00-2E/device.crt"));
 
 
 //
