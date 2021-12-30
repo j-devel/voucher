31948dceea7e44d2b84fce553a2c380acf162415
j-devel <j@w3reality.com>
2021-12-30 12:05:42 +0700

    Get `test_{unsigned,validate,sign}_vrq_f2_00_02()` to work

--- src/tests.rs
+++ src/tests.rs
@@ -20,13 +20,11 @@ static VRQ_F2_00_02: &[u8] = core::include_bytes!(
 static MASA_CRT_F2_00_02: &[u8] = core::include_bytes!(
     concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/masa.crt"));
 
-// DEPRECATED !!
-static KEY_PEM_02_00_2E: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-02-00-2E/key.pem"));
+static DEVICE_CRT_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/device.crt"));
 
-// DEPRECATED !!
-static DEVICE_CRT_02_00_2E: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-02-00-2E/device.crt"));
+static KEY_PEM_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/key.pem"));
 
 //
 
@@ -129,25 +127,41 @@ fn test_serialize_vch_f2_00_02() {
 
 //
 
+fn build_vrq_f2_00_02() -> Voucher {
+    let mut vrq = Voucher::new(VoucherType::Vrq);
+    vrq.set_sid(Sid::VrqAssertion(Yang::Enumeration(YangEnum::Proximity)))
+        .set_sid(Sid::VrqCreatedOn(Yang::DateAndTime(1599086034)))
+        .set_sid(Sid::VrqNonce(Yang::Binary(vec![48, 130, 1, 216, 48, 130, 1, 94, 160, 3, 2, 1, 2, 2, 1, 1, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4, 3, 2, 48, 115, 49, 18, 48, 16, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 2, 99, 97, 49, 25, 48, 23, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 9, 115, 97, 110, 100, 101, 108, 109, 97, 110, 49, 66, 48, 64, 6, 3, 85, 4, 3, 12, 57, 35, 60, 83, 121, 115, 116, 101, 109, 86, 97, 114, 105, 97, 98, 108, 101, 58, 48, 120, 48, 48, 48, 48, 53, 53, 98, 56, 50, 53, 48, 99, 48, 100, 98, 56, 62, 32, 85, 110, 115, 116, 114, 117, 110, 103, 32, 70, 111, 117, 110, 116, 97, 105, 110, 32, 67, 65, 48, 30, 23, 13, 50, 48, 48, 56, 50, 57, 48, 52, 48, 48, 49, 54, 90, 23, 13, 50, 50, 48, 56, 50, 57, 48, 52, 48, 48, 49, 54, 90, 48, 70, 49, 18, 48, 16, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 2, 99, 97, 49, 25, 48, 23, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 9, 115, 97, 110, 100, 101, 108, 109, 97, 110, 49, 21, 48, 19, 6, 3, 85, 4, 3, 12, 12, 85, 110, 115, 116, 114, 117, 110, 103, 32, 74, 82, 67, 48, 89, 48, 19, 6, 7, 42, 134, 72, 206, 61, 2, 1, 6, 8, 42, 134, 72, 206, 61, 3, 1, 7, 3, 66, 0, 4, 150, 101, 80, 114, 52, 186, 159, 229, 221, 230, 95, 246, 240, 129, 111, 233, 72, 158, 129, 12, 18, 7, 59, 70, 143, 151, 100, 43, 99, 0, 141, 2, 15, 87, 201, 124, 148, 127, 132, 140, 178, 14, 97, 214, 201, 136, 141, 21, 180, 66, 31, 215, 242, 106, 183, 228, 206, 5, 248, 167, 76, 211, 139, 58, 163, 16, 48, 14, 48, 12, 6, 3, 85, 29, 19, 1, 1, 255, 4, 2, 48, 0, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4, 3, 2, 3, 104, 0, 48, 101, 2, 49, 0, 135, 158, 205, 227, 138, 5, 18, 46, 182, 247, 44, 178, 27, 195, 210, 92, 190, 230, 87, 55, 112, 86, 156, 236, 35, 12, 164, 140, 57, 241, 64, 77, 114, 212, 215, 85, 5, 155, 128, 130, 2, 14, 212, 29, 79, 17, 159, 231, 2, 48, 60, 20, 216, 138, 10, 252, 64, 71, 207, 31, 135, 184, 115, 193, 106, 40, 191, 184, 60, 15, 136, 67, 77, 157, 243, 247, 168, 110, 45, 198, 189, 136, 149, 68, 47, 32, 55, 237, 204, 228, 133, 91, 17, 218, 154, 25, 228, 232])))
+        .set_sid(Sid::VrqProximityRegistrarCert(Yang::Binary(vec![102, 114, 118, 85, 105, 90, 104, 89, 56, 80, 110, 86, 108, 82, 75, 67, 73, 83, 51, 113, 77, 81])))
+        .set_sid(Sid::VrqSerialNumber(Yang::String("00-D0-E5-F2-00-02".as_bytes().to_vec())));
+
+    vrq
+}
+
 #[test]
-fn test_pledge_vr_unsigned_02_00_2e() {
-//fn test_unsigned_vrq_f2_00_02() { // !! TODO
+fn test_unsigned_vrq_f2_00_02() {
     #[cfg(feature = "v3")]
     init_psa_crypto();
 
-    let mut vrq = Voucher::new(VoucherType::Vrq);
-
-    vrq.set_sid(Sid::VrqAssertion(Yang::Enumeration(YangEnum::Proximity)))
-        .set_sid(Sid::VrqCreatedOn(Yang::DateAndTime(1635218340)))
-        .set_sid(Sid::VrqNonce(Yang::Binary(vec![114, 72, 103, 99, 66, 86, 78, 86, 97, 70, 109, 66, 87, 98, 84, 77, 109, 101, 79, 75, 117, 103])))
-        .set_sid(Sid::VrqSerialNumber(Yang::String("00-D0-E5-02-00-2E".as_bytes().to_vec())));
+    let mut vrq = build_vrq_f2_00_02();
 
     // "validating an unsigned voucher" should fail
-    assert!(vrq.validate(Some(DEVICE_CRT_02_00_2E)).is_err());
+    assert!(vrq.validate(Some(DEVICE_CRT_F2_00_02)).is_err());
 
-    vrq.sign(KEY_PEM_02_00_2E, SignatureAlgorithm::ES256).unwrap();
+    vrq.sign(KEY_PEM_F2_00_02, SignatureAlgorithm::ES256).unwrap();
 
-    assert!(vrq.validate(Some(DEVICE_CRT_02_00_2E)).is_ok());
+    assert!(vrq.validate(Some(DEVICE_CRT_F2_00_02)).is_ok());
+}
+
+#[test]
+fn test_validate_vrq_f2_00_02() {
+    #[cfg(feature = "v3")]
+    init_psa_crypto();
+
+    assert!(Voucher::try_from(VRQ_F2_00_02)
+        .unwrap()
+        .validate(Some(DEVICE_CRT_F2_00_02))
+        .is_ok());
 }
 
 #[test]
@@ -155,26 +169,19 @@ fn test_sign_vrq_f2_00_02() {
     #[cfg(feature = "v3")]
     init_psa_crypto();
 
-    let mut vrq = Voucher::new(VoucherType::Vrq);
+    let mut vrq = build_vrq_f2_00_02();
 
     assert!(vrq
-        .set_sid(Sid::VrqAssertion(Yang::Enumeration(YangEnum::Proximity)))
-        .set_sid(Sid::VrqCreatedOn(Yang::DateAndTime(1599086034)))
-        .set_sid(Sid::VrqNonce(Yang::Binary(vec![48, 130, 1, 216, 48, 130, 1, 94, 160, 3, 2, 1, 2, 2, 1, 1, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4, 3, 2, 48, 115, 49, 18, 48, 16, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 2, 99, 97, 49, 25, 48, 23, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 9, 115, 97, 110, 100, 101, 108, 109, 97, 110, 49, 66, 48, 64, 6, 3, 85, 4, 3, 12, 57, 35, 60, 83, 121, 115, 116, 101, 109, 86, 97, 114, 105, 97, 98, 108, 101, 58, 48, 120, 48, 48, 48, 48, 53, 53, 98, 56, 50, 53, 48, 99, 48, 100, 98, 56, 62, 32, 85, 110, 115, 116, 114, 117, 110, 103, 32, 70, 111, 117, 110, 116, 97, 105, 110, 32, 67, 65, 48, 30, 23, 13, 50, 48, 48, 56, 50, 57, 48, 52, 48, 48, 49, 54, 90, 23, 13, 50, 50, 48, 56, 50, 57, 48, 52, 48, 48, 49, 54, 90, 48, 70, 49, 18, 48, 16, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 2, 99, 97, 49, 25, 48, 23, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 9, 115, 97, 110, 100, 101, 108, 109, 97, 110, 49, 21, 48, 19, 6, 3, 85, 4, 3, 12, 12, 85, 110, 115, 116, 114, 117, 110, 103, 32, 74, 82, 67, 48, 89, 48, 19, 6, 7, 42, 134, 72, 206, 61, 2, 1, 6, 8, 42, 134, 72, 206, 61, 3, 1, 7, 3, 66, 0, 4, 150, 101, 80, 114, 52, 186, 159, 229, 221, 230, 95, 246, 240, 129, 111, 233, 72, 158, 129, 12, 18, 7, 59, 70, 143, 151, 100, 43, 99, 0, 141, 2, 15, 87, 201, 124, 148, 127, 132, 140, 178, 14, 97, 214, 201, 136, 141, 21, 180, 66, 31, 215, 242, 106, 183, 228, 206, 5, 248, 167, 76, 211, 139, 58, 163, 16, 48, 14, 48, 12, 6, 3, 85, 29, 19, 1, 1, 255, 4, 2, 48, 0, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4, 3, 2, 3, 104, 0, 48, 101, 2, 49, 0, 135, 158, 205, 227, 138, 5, 18, 46, 182, 247, 44, 178, 27, 195, 210, 92, 190, 230, 87, 55, 112, 86, 156, 236, 35, 12, 164, 140, 57, 241, 64, 77, 114, 212, 215, 85, 5, 155, 128, 130, 2, 14, 212, 29, 79, 17, 159, 231, 2, 48, 60, 20, 216, 138, 10, 252, 64, 71, 207, 31, 135, 184, 115, 193, 106, 40, 191, 184, 60, 15, 136, 67, 77, 157, 243, 247, 168, 110, 45, 198, 189, 136, 149, 68, 47, 32, 55, 237, 204, 228, 133, 91, 17, 218, 154, 25, 228, 232])))
-        .set_sid(Sid::VrqProximityRegistrarCert(Yang::Binary(vec![102, 114, 118, 85, 105, 90, 104, 89, 56, 80, 110, 86, 108, 82, 75, 67, 73, 83, 51, 113, 77, 81])))
-        .set_sid(Sid::VrqSerialNumber(Yang::String("00-D0-E5-F2-00-02".as_bytes().to_vec())))
-        .sign(KEY_PEM_02_00_2E, SignatureAlgorithm::ES256) // !! todo KEY_PEM_F2_00_02
+        .sign(KEY_PEM_F2_00_02, SignatureAlgorithm::ES256)
         .unwrap()
-        .validate(Some(DEVICE_CRT_02_00_2E)) // via public key // !! todo _F2_00_02
+        .validate(Some(DEVICE_CRT_F2_00_02)) // via public key
         .is_ok());
 
-    assert!(vrq.validate(Some(KEY_PEM_02_00_2E)).is_ok()); // via private key // !! todo _F2_00_02
+    assert!(vrq.validate(Some(KEY_PEM_F2_00_02)).is_ok()); // via private key
 
-    let vrq_gt = Voucher::try_from(VRQ_F2_00_02).unwrap();
     assert!(debug::content_comp_permissive(
-        &vrq_gt.get_content_debug().unwrap(),
-        &vrq.get_content_debug().unwrap()));
-
+        &vrq.get_content_debug().unwrap(),
+        &Voucher::try_from(VRQ_F2_00_02).unwrap().get_content_debug().unwrap()));
 
     let (sig, ty) = vrq.get_signature();
     assert!(sig.len() > 0);
@@ -182,28 +189,14 @@ fn test_sign_vrq_f2_00_02() {
 }
 
 //#[test]
-fn test_pledge_vr_validate_02_00_2e() {
-//fn test_validate_vrq_f2_00_02() { // !! TODO
+fn test_serialize_vrq_f2_00_02() {
     #[cfg(feature = "v3")]
     init_psa_crypto();
 
-    // assert!(Voucher::try_from(debug::COSE_VRQ_02_00_2E)
-    //     .unwrap()
-    //     .validate(Some(DEVICE_CRT_02_00_2E))
-    //     .is_ok());
-}
-
-//#[test]
-fn test_pledge_vr_serialize_02_00_2e() {
-//fn test_serialize_vrq_f2_00_02() { // !! TODO
-    #[cfg(feature = "v3")]
-    init_psa_crypto();
-
-//    let vrq = Voucher::try_from(debug::COSE_VRQ_02_00_2E).unwrap();
-//    assert_eq!(vrq.get_content_debug().unwrap(),
-//               debug::CONTENT_VRQ_02_00_2E);
-//    assert_eq!(vrq.get_signature().0, /* bare */ [213, 235, 111, 50, 190, 110, 39, 125, 24, 10, 108, 112, 208, 115, 138, 149, 12, 183, 237, 34, 220, 209, 168, 239, 185, 5, 170, 145, 221, 42, 135, 70, 13, 231, 183, 48, 88, 32, 174, 78, 146, 46, 72, 206, 11, 103, 80, 17, 80, 62, 17, 101, 155, 78, 7, 1, 87, 177, 172, 192, 118, 31, 116, 214]);
-//    assert_eq!(vrq.serialize().unwrap(), debug::COSE_VRQ_02_00_2E);
+   let vrq = Voucher::try_from(VRQ_F2_00_02).unwrap();
+   // assert_eq!(vrq.get_content_debug().unwrap(), todo !!!!);
+   assert_eq!(vrq.get_signature().0, /* bare */ [242, 113, 238, 15, 125, 71, 169, 233, 252, 219, 95, 74, 88, 238, 47, 97, 183, 138, 84, 131, 159, 203, 164, 31, 34, 135, 174, 129, 228, 47, 180, 129, 171, 146, 165, 162, 167, 222, 82, 112, 125, 198, 7, 254, 142, 250, 108, 214, 194, 253, 235, 104, 154, 68, 171, 179, 127, 93, 192, 158, 174, 24, 23, 8]);
+   assert_eq!(vrq.serialize().unwrap(), VRQ_F2_00_02);
 }
 
 //
@@ -215,20 +208,21 @@ fn test_highlevel_interface() {
 
     let mut vrq = Voucher::new_vrq();
 
-    assert!(vrq
-        .set(Attr::Assertion(Assertion::Proximity))
-        .set(Attr::CreatedOn(1635218340))
-        .set(Attr::Nonce(vec![114, 72, 103, 99, 66, 86, 78, 86, 97, 70, 109, 66, 87, 98, 84, 77, 109, 101, 79, 75, 117, 103]))
-        .set(Attr::SerialNumber(String::from("00-D0-E5-02-00-2E")))
-        .sign(KEY_PEM_02_00_2E, SignatureAlgorithm::ES256)
-        .unwrap()
-        .validate(Some(DEVICE_CRT_02_00_2E))
-        .is_ok());
-
-    // assert!(debug::content_comp(&vrq.get_content_debug().unwrap(),
-    //                             debug::CONTENT_VRQ_02_00_2E));
-    assert_eq!(vrq.get_signature().0, /* asn1 */ [48, 69, 2, 32, 110, 143, 135, 7, 170, 12, 231, 167, 243, 130, 212, 214, 122, 23, 71, 106, 100, 76, 173, 196, 236, 73, 58, 126, 151, 8, 46, 127, 206, 190, 196, 66, 2, 33, 0, 217, 20, 0, 2, 48, 18, 151, 42, 133, 159, 125, 145, 86, 197, 138, 227, 30, 64, 230, 164, 214, 125, 78, 62, 183, 48, 179, 249, 79, 147, 36, 112]);
-    assert_eq!(vrq.serialize().unwrap().len(), 148);
+    // todo !!!!
+    // assert!(vrq
+    //     .set(Attr::Assertion(Assertion::Proximity))
+    //     .set(Attr::CreatedOn(1635218340))
+    //     .set(Attr::Nonce(vec![114, 72, 103, 99, 66, 86, 78, 86, 97, 70, 109, 66, 87, 98, 84, 77, 109, 101, 79, 75, 117, 103]))
+    //     .set(Attr::SerialNumber(String::from("00-D0-E5-02-00-2E")))
+    //     .sign(KEY_PEM_02_00_2E, SignatureAlgorithm::ES256)
+    //     .unwrap()
+    //     .validate(Some(DEVICE_CRT_02_00_2E))
+    //     .is_ok());
+    //
+    // // assert!(debug::content_comp(&vrq.get_content_debug().unwrap(),
+    // //                             debug::CONTENT_VRQ_02_00_2E));
+    // assert_eq!(vrq.get_signature().0, /* asn1 */ [48, 69, 2, 32, 110, 143, 135, 7, 170, 12, 231, 167, 243, 130, 212, 214, 122, 23, 71, 106, 100, 76, 173, 196, 236, 73, 58, 126, 151, 8, 46, 127, 206, 190, 196, 66, 2, 33, 0, 217, 20, 0, 2, 48, 18, 151, 42, 133, 159, 125, 145, 86, 197, 138, 227, 30, 64, 230, 164, 214, 125, 78, 62, 183, 48, 179, 249, 79, 147, 36, 112]);
+    // assert_eq!(vrq.serialize().unwrap().len(), 148);
 
     /* WIP */ assert_eq!(vrq.get(ATTR_CREATED_ON), Some(Attr::CreatedOn(1635218340)));
 
