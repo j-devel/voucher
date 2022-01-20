48dd920dfd2f698809075a2a00556845ecbd1ffb
j-devel <j@w3reality.com>
2022-01-20 13:10:07 +0700

    Docs: Add examples for `.get_signature()`

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["minerva_voucher"] = {"name":"","files":["attr.rs","cose_data.rs","cose_sig.rs","lib.rs","sid.rs","sid_data.rs","sign.rs","utils.rs","validate.rs","yang.rs"]};
+sourcesIndex["minerva_voucher"] = {"name":"","files":["attr.rs","cose_data.rs","cose_sig.rs","lib.rs","sid.rs","sid_data.rs","sign.rs","utils.rs","validate.rs","voucher.rs","yang.rs"]};
 createSourceSidebar();
